055e2ce8986e86f6b6716df4c232d5941dbbea41
jseraf <j-serafin@northwestern.edu>
2021-08-20 21:59:08 -0500

    Use correct scopes

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,6 @@
 class LabelsController < ApplicationController
   def show
     @label = Label.find(params[:id])
-    @pagy, @spins = pagy(Spin.where(label: @label).with_artist_release)
+    @pagy, @spins = pagy(Spin.where(label: @label).with_artist.with_release)
   end
 end
