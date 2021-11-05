bf849ea78ba20dfd0c607bda4e31fb75e0b2db2e
jseraf <j-serafin@northwestern.edu>
2021-11-04 20:33:26 -0500

    WIP: use shared css tables

--- app/controllers/spins/search_controller.rb
+++ app/controllers/spins/search_controller.rb
@@ -1,9 +1,10 @@
 module Spins
   class SearchController < SpinsController
     def search
-      @q = Spin.with_artist_release_label.with_dj.recent.ransack(params[:q])
+      @q            = Spin.with_artist_release_label.with_dj.recent.ransack(params[:q])
       @pagy, @spins = pagy(@q.result)
       @spins        = SpinDecorator.decorate_collection(@spins)
+
       render 'spins/index'
     end
   end
